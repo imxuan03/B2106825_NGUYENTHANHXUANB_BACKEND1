88057f78c133c1f1514c0c810eb5ffd3e6d0bd2e
imxuan03 <txuan0164@gmail.com>
2024-02-01 11:20:17 +0700

    first commit

---
+++ app/controllers/contact.controller.js
@@ -0,0 +1,41 @@
+exports.create = (req, res) => {
+    res.send({
+        message: " Create Handler"
+    })
+};
+
+exports.findAll = (req, res) => {
+    res.send({
+        message: " FindAll Handler"
+    })
+};
+
+exports.findOne = (req, res) => {
+    res.send({
+        message: " FindOne Handler"
+    })
+};
+
+exports.update = (req, res) => {
+    res.send({
+        message: " Update Handler"
+    })
+};
+
+exports.delete = (req, res) => {
+    res.send({
+        message: " Delete Handler"
+    })
+};
+
+exports.deleteAll = (req, res) => {
+    res.send({
+        message: " DeleteAll Handler"
+    })
+};
+
+exports.findAllFavorite = (req, res) => {
+    res.send({
+        message: " findAllFavorite Handler"
+    })
+};
